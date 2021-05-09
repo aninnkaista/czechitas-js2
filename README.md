4a02619dcea221144386a069cff8a874fc68218f
Anna Istomina <aninnkasta@gmail.com>
2021-05-09 12:49:56 +0200

    5th lecture including homework 2

---
+++ README.md
@@ -0,0 +1,5 @@
+# Homework_2
+* ### js tasks (1 and 2) are in main.js;
+* ### sccs task is in style.scss;
+* ### parts of code related to home tasks are commented accordingly;
+* ### I was not able to participate in the 5th lecture and was filling code by watching video so this part can slightly differ.
--- main.js
+++ main.js
@@ -1,24 +1,70 @@
-const header = document.querySelector('header.header-news');
+const header = document.querySelector(
+    'header.header-news > div.header-news__container',
+);
+// carousel settings
+const carouselItemCount = 3;
+let carouselItemStart = 0;
+let articles;
 
-//data with get method from js server
+//carousel buttons
+const buttonLeft = document.getElementById('carousel-button-left');
+const buttonRight = document.getElementById('carousel-button-right');
+
+//----------------------------------task 2 to limit buttons---------------------
+function checkButtonVisibity(itemStart, numberTotalItems) {
+    if (itemStart === 0) {
+        buttonLeft.hidden = true;
+    } else if (itemStart >= numberTotalItems - carouselItemCount) {
+        buttonRight.hidden = true;
+    } else {
+        buttonLeft.hidden = false;
+        buttonRight.hidden = false;
+    }
+}
+//-----------------------------------task 2--------------------------------------
+
+// data with get method from js server
 fetch('http://localhost:3000/news.json')
     .then((serverResponse) => serverResponse.text())
     .then((responseText) => {
         const data = JSON.parse(responseText);
-        populateNewsCarousel(data.articles);
+        articles = data.articles;
+        populateNewsCarousel(data.articles, carouselItemStart);
     });
 
-function populateNewsCarousel(news) {
-    for (let i = 0; i < news.length; i++) {
+// to populate carousel with news articles
+function populateNewsCarousel(news, startAt) {
+    //----------------------------task 2 to empty header-------------------------
+    header.innerText = '';
+    //----------------------------task 2-----------------------------------------
+    for (let i = startAt; i < startAt + carouselItemCount; i++) {
         // to create an element
         const newsValue = news[i];
         const newsDiv = createDivForNews(newsValue);
         header.appendChild(newsDiv);
     }
+    checkButtonVisibity(startAt, news.length);
 }
 
-function createDivForNews(newsContent) {
+// each news article into separate div
+function createDivForNews(newsContents) {
     const newsArticle = document.createElement('div');
-    newsArticle.innerText = newsContent.title;
+    newsArticle.classList.add('news-article');
+    // to make background as newsarticle
+    newsArticle.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.7), transparent), url(${newsContents.image})`;
+
+    const title = document.createElement('span');
+    title.classList.add('news-article__title');
+    title.innerHTML = newsContents.title;
+    newsArticle.appendChild(title);
     return newsArticle;
 }
+
+buttonLeft.addEventListener('click', () => {
+    carouselItemStart--;
+    populateNewsCarousel(articles, carouselItemStart);
+});
+buttonRight.addEventListener('click', () => {
+    carouselItemStart++;
+    populateNewsCarousel(articles, carouselItemStart);
+});
